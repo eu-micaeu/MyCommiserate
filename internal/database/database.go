84ea55e0f506c25ad9cdccce17c377a7573f08c9
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-07-22 12:51:45 -0300

    Organização no código da API.

--- internal/database/database.go
+++ internal/database/database.go
@@ -7,20 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type User struct {
-	ID_User  int    `json:"id_usuario"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
-type Anot struct {
-	ID_Anot  int    `json:"id_anotacao"`
-	ID_User  int    `json:"id_usuario"`
-	Titulo   string `json:"titulo"`
-	Anotacao string `json:"anotacao"`
-}
-
-func database() {
+func dbConn() {
 	dbUser := "root"
 	dbPassword := "ajCPqarJKpcy6cdvrAHF"
 	dbHost := "containers-us-west-83.railway.app"
@@ -29,11 +16,6 @@ func database() {
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
 	if err != nil {
 		log.Fatal(err)
 	}
