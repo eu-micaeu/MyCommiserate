e236fad990ee196025ff5c5b321454fbfaab6dab
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-07-22 15:45:59 -0300

    Adicionando a Trigger no banco para BACKUP.

--- script.sql
+++ script.sql
@@ -14,3 +14,20 @@ CREATE TABLE anotacoes (
     anotacao TEXT,
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
+
+CREATE TABLE backup_anotacoes (
+    id_anotacao int,
+    id_usuario int,
+    titulo TEXT,
+    anotacao TEXT
+);
+
+DELIMITER //
+CREATE TRIGGER backup_anotacoes_trigger
+BEFORE DELETE ON anotacoes
+FOR EACH ROW
+BEGIN
+    INSERT INTO backup_anotacoes VALUES (OLD.id_anotacao, OLD.id_usuario, OLD.titulo, OLD.anotacao);
+END;//
+DELIMITER ;
+
