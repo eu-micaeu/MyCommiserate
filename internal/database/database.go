46bc1f338d8f663a95b70156f4dd6e011988de04
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-07-24 16:45:27 -0300

    Configurando Env.

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,7 +3,6 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
@@ -17,11 +16,11 @@ func NewDB() (*sql.DB, error) {
         return nil, err
     }
 
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
+	dbUser := "DB_USER"
+	dbPassword := "DB_PASSWORD"
+	dbHost := "DB_HOST"
+	dbPort := "DB_PORT"
+	dbName := "DB_NAME"
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 	db, err := sql.Open("mysql", dsn)
