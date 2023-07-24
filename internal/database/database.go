e8b6f41e379f9d2444d09452ae58287d0532a8ba
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-07-24 17:12:09 -0300

    Configurando as vars. de ambiente.

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,32 +3,23 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
 func NewDB() (*sql.DB, error) {
-
-	err := godotenv.Load()
-    if err != nil {
-        fmt.Println("Erro ao carregar o arquivo .env:", err)
-        return nil, err
-    }
-
-	dbUser := "DB_USER"
-	dbPassword := "DB_PASSWORD"
-	dbHost := "DB_HOST"
-	dbPort := "DB_PORT"
-	dbName := "DB_NAME"
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, err
 	}
-
-
 
 	return db, nil
 }
